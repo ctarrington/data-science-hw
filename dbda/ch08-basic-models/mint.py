e52a9e0968c4ed64f31f97fda39c13b3d1a609bc
CT Arrington <arrington.ct@gmail.com>
2020-07-07 21:41:09 -0400

    Update mint.py

--- dbda/ch08-basic-models/mint.py
+++ dbda/ch08-basic-models/mint.py
@@ -20,3 +20,21 @@ class Mint:
       return coins
 
     target.extend(coins)
+    
+    
+class Coin:
+  coin_ctr = 0
+
+  def __init__(self, probability, mint_id):
+    self.id = Coin.coin_ctr
+    Coin.coin_ctr = Coin.coin_ctr + 1
+    self.mint_id = mint_id
+    self.bernoulli_rv = bernoulli(probability)
+    
+
+  def flip(self, target = None):
+    response = [self.mint_id, self.id, self.bernoulli_rv.rvs(1)[0]]
+    if target is None:
+      return response
+
+    target.append(response)
