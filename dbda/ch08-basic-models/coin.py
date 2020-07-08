f12170380fbcc1da3f6d9097c7cece372659884c
CT Arrington <arrington.ct@gmail.com>
2020-07-07 21:29:47 -0400

    Create coin.py

---
+++ dbda/ch08-basic-models/coin.py
@@ -0,0 +1,16 @@
+class Coin:
+  coin_ctr = 0
+
+  def __init__(self, probability, mint_id):
+    self.id = Coin.coin_ctr
+    Coin.coin_ctr = Coin.coin_ctr + 1
+    self.mint_id = mint_id
+    self.bernoulli_rv = bernoulli(probability)
+    
+
+  def flip(self, target = None):
+    response = [self.mint_id, self.id, self.bernoulli_rv.rvs(1)[0]]
+    if target is None:
+      return response
+
+    target.append(response)
