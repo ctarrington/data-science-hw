8034bdbda566e5bec2cf59981824f54666c8ced2
ctarrington <arrington.ct@gmail.com>
2020-08-05 22:44:15 -0400

    early noise

--- kalman-filters/one-dimension/ballistic_data.py
+++ kalman-filters/one-dimension/ballistic_data.py
@@ -1,21 +1,27 @@
 import numpy as np
 
 class BallisticData:
-    def __init__(self, initial_x, initial_x_dot, acceleration_function, noise = 1, steps_per_iteration = 500, time_per_iteration = 1, max_iterations = 5, seed = 123):
-        self.x = initial_x
-        self.x_dot = initial_x_dot
+    def __init__(self, initial_x, initial_x_dot, acceleration_function, noise = (1,1),
+                 steps_per_iteration = 500, time_per_iteration = 1, max_iterations = 5, seed = 123):
+        self.initial_x = initial_x
+        self.initial_x_dot = initial_x_dot
         self.acceleration_function = acceleration_function
+        self.noise_x = noise[0]
+        self.noise_x_dot = noise[1]
         self.steps_per_iteration = steps_per_iteration
         self.time_per_iteration = time_per_iteration
         self.max_iterations = max_iterations
         self.seed = seed
 
-        self.step = 0
-        self.iterations = 0
         self.step_time = time_per_iteration / steps_per_iteration
 
 
     def __iter__(self):
+        self.x = self.initial_x
+        self.x_dot = self.initial_x_dot
+        np.random.seed(self.seed)
+        self.step = 0
+        self.iterations = 0
         return self
 
     def __increment_step(self):
@@ -27,9 +33,14 @@ class BallisticData:
         self.x = self.x + adjusted_x_dot * self.step_time
         self.step = self.step + 1
 
+    def __x_noise(self):
+        return np.random.normal(0, self.noise_x, 1)[0]
+
+    def __x_dot_noise(self):
+        return np.random.normal(0, self.noise_x_dot, 1)[0]
 
     def __next__(self):
-        column_vector = np.array([[self.x], [self.x_dot]])
+        column_vector = np.array([[self.x + self.__x_noise()], [self.x_dot + self.__x_dot_noise()]])
 
         self.iterations = self.iterations + 1
         if self.iterations == self.max_iterations:
--- kalman-filters/one-dimension/ballistic_samples.py
+++ kalman-filters/one-dimension/ballistic_samples.py
@@ -5,21 +5,56 @@ import pandas as pd
 from ballistic_filter import BallisticFilter
 from ballistic_data import BallisticData
 
+x_std = 1
+x_dot_std = 2
+
 filter = BallisticFilter(acceleration_x=2,
                          time_step_size=1,
                          process_error_x=20,
                          process_error_dx=5,
-                         observation_error_x=25,
-                         observation_error_dx=6,
+                         observation_error_x=x_std**2,
+                         observation_error_dx=x_dot_std**2,
                          verbose=False)
 
 
-observations = BallisticData(0, 0, lambda t: 2)
+observations = BallisticData(initial_x=0,
+                             initial_x_dot=0,
+                             acceleration_function=lambda x: 2,
+                             noise=(x_std, x_dot_std),
+                             max_iterations=20)
 
-df = pd.DataFrame()
+noiseless_observations = BallisticData(initial_x=0,
+                             initial_x_dot=0,
+                             acceleration_function=lambda x: 2,
+                             noise=(0, 0),
+                             max_iterations=20)
+
+kalmanized_df = pd.DataFrame()
 for observation in observations:
     state_row = filter.iterate(observation)
-    df = df.append(pd.DataFrame(state_row), ignore_index=True)
+    kalmanized_df = kalmanized_df.append(pd.DataFrame(state_row), ignore_index=True)
 
-df.columns=['seconds', 'x', 'x_dot']
-print('df\n', df)
+kalmanized_df.columns=['seconds', 'x', 'x_dot']
+print('kalmanized_df\n', kalmanized_df )
+
+actual_df = pd.DataFrame()
+seconds = 0
+for observation in noiseless_observations:
+    state_row = np.array([seconds, observation[0][0], observation[1][0]])
+    state_row.shape = (1,3)
+    actual_df = actual_df.append(pd.DataFrame(state_row), ignore_index=True)
+    seconds = seconds + 1
+
+actual_df.columns=['seconds', 'x', 'x_dot']
+print('actual_df\n', actual_df)
+
+observed_df = pd.DataFrame()
+seconds = 0
+for observation in observations:
+    state_row = np.array([seconds, observation[0][0], observation[1][0]])
+    state_row.shape = (1,3)
+    observed_df = observed_df.append(pd.DataFrame(state_row), ignore_index=True)
+    seconds = seconds + 1
+
+observed_df.columns=['seconds', 'x', 'x_dot']
+print('observed_df\n', observed_df)
