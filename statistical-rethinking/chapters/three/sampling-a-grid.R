3aeffb36871417c5ad7a6d1eee8a4a54818ce0c2
CT Arrington <arrington.ct@gmail.com>
2022-03-12 14:50:45 -0500

    sampling and intervals

--- statistical-rethinking/chapters/three/sampling-a-grid.R
+++ statistical-rethinking/chapters/three/sampling-a-grid.R
@@ -6,12 +6,19 @@ likelihood <- dbinom(6, size=9, prob=p_grid)
 plot(likelihood)
 
 posterior <- likelihood * prior
-plot(posterior)
 posterior <- posterior / sum(posterior)
-plot(posterior)
 
-samples <- sample(p_grid, prob=posterior, size=1e5, replace = TRUE)
-plot(samples)
-hist(samples)
-plot(density(samples))
+samples <- sample(p_grid, prob=posterior, size=1e4, replace = TRUE)
+dens(samples)
 
+direct_from_distribution <- sum(posterior[p_grid < 0.5])
+from_sample <- sum(samples < 0.5) / 1e4
+
+middle_eighty <- quantile(samples, c(0.1, 0.9))
+
+# find the limits of a Highest Posterior Density Interval
+# smallest part of domain that contains the desired area
+# of the probability distribution
+limits <- HPDI(samples, prob = 0.8)
+sum(posterior[p_grid > limits[1] & p_grid < limits[2]])
+sum(samples > limits[1] & samples < limits[2]) / 1e4
