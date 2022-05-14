74d8770e0d05648e25c2c75a3ee7d400253d0453
CT Arrington <arrington.ct@gmail.com>
2022-05-14 11:47:34 -0400

    fit models, compare models

--- ucsc-bayesian/course2-intro-mcmc/w3-linear-regression.R
+++ ucsc-bayesian/course2-intro-mcmc/w3-linear-regression.R
@@ -62,3 +62,43 @@ mod1_sim = coda.samples(model=mod1,
 mod1_csim = do.call(rbind, mod1_sim) # combine multiple chains
 summary(mod1_csim)
 
+plot(mod1_sim)
+gelman.diag(mod1_sim)
+gelman.plot(mod1_sim)
+autocorr.diag(mod1_sim)
+autocorr.plot(mod1_sim)
+effectiveSize(mod1_sim)
+summary(mod1_sim)
+
+# compare to lmod
+summary(lmod)
+
+# how do you evaluate a model from the residuals?
+# what does it look like if you pick the wrong model
+nolog_mod = lm(infant ~ income, data = cleaned)
+
+plot(resid(nolog_mod)) # to check independence (looks okay)
+plot(predict(nolog_mod), resid(nolog_mod)) # to check for linearity, constant variance (looks bad)
+qqnorm(resid(nolog_mod)) # to check Normality assumption (we want this to be a straight line)
+# so our eyes did not betray us - linear is the wrong model to fit
+
+# how did we do with y=x^a ??
+# We want to compare the residual between each actual value
+# and the predicted value from the model
+
+# we want to do vector math for infant_predicted = b1 + b2 * income_data
+# maybe stare at the pieces of this for a minute...
+X = cbind(rep(1.0, data1_jags$n), data1_jags$log_income)
+
+pm_params1 = colMeans(mod1_csim) # posterior mean
+
+# multiply for each row to get a predicted value for log(infant mortality)
+yhat1 = drop(X %*% pm_params1[1:2])
+resid1 = data1_jags$y - yhat1
+plot(resid1) # against data index
+qqnorm(resid1) # checking normality of residuals
+
+plot(yhat1, resid1) # against predicted values
+plot(predict(lmod), resid(lmod)) # to compare with reference linear model
+plot(predict(nolog_mod), resid(nolog_mod)) # to compare with no log linear model
+
