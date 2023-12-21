de3cdbe4657b2a22b6a70aa9a1f22e63ce2962eb
parallels <arrington.ct@gmail.com>
2023-12-21 11:56:27 -0500

    scraps and scripts

--- scripts/launch-jupyter.sh
+++ scripts/launch-jupyter.sh
@@ -16,7 +16,7 @@ gid=${gid:-$(id -g)}
 subuidSize=$(( $(podman info --format "{{ range .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 subgidSize=$(( $(podman info --format "{{ range .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 
-podman run -it --rm -p 10000:8888 \
+podman run -it --rm -p 8888:8888 \
     -v "${PWD}:/home/jovyan/work:Z" --user $uid:$gid \
     --uidmap $uid:0:1 --uidmap 0:1:$uid --uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
     --gidmap $gid:0:1 --gidmap 0:1:$gid --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
